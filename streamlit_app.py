58d16b39bdc26000f4d0d3c92e589470fa2e484c
hamiparty003 <141626972+hamiparty003@users.noreply.github.com>
2023-08-08 10:33:28 +0800

    Update streamlit_app.py
    change the display of dataframe based on the selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,12 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # let's put a pick list here so they can pick the friut they want
-streamlit.multiselect("Pick Some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
+#streamlit.multiselect("Pick Some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
+
+fruits_selected = streamlit.multiselect("Pick Some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display
-streamlit.dataframe(my_fruit_list)
+# streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
+
