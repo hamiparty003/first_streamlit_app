c7247729287e523ed88180571be8aa10aa4009a6
hamiparty003 <141626972+hamiparty003@users.noreply.github.com>
2023-08-09 15:24:22 +0800

    Update streamlit_app.py
    change text to dataframe for better format

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,11 @@ my_cur = my_cnx.cursor()
 # my_data_row = my_cur.fetchone()
 # streamlit.text("Hello from Snowflake:")
 # streamlit.text(my_data_row)
+
 my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit load list contains:")
+#streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
+
+
