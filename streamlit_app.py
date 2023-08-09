489d8e4004b82e4c08ef2db8408f36601b1986a9
hamiparty003 <141626972+hamiparty003@users.noreply.github.com>
2023-08-09 16:58:32 +0800

    Update streamlit_app.py
    define function

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,11 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
-
 streamlit.header('Breakfast Menu')
-
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
   
 ## import pandas
@@ -23,31 +20,33 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # let's put a pick list here so they can pick the friut they want
 #streamlit.multiselect("Pick Some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
-
 fruits_selected = streamlit.multiselect("Pick Some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# display
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
-
 streamlit.header('Fruityvice Fruit Advice!')
-
 ##import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # streamlit.text(fruityvice_response.json())
 
+# define the function
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  #normalize json format to dataframe/table
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+# Error exception handling
 try:
   #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    #normalize json format to dataframe/table
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
   
