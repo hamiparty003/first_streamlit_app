a4040c53e856ea956d5ee1e31a0e365c08071415
hamiparty003 <141626972+hamiparty003@users.noreply.github.com>
2023-08-09 17:09:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,9 +69,6 @@ if streamlit.button('Get Fruit Load List'):
   my_data_row = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-#streamlit.text(my_data_row)
-streamlit.dataframe(my_data_row)
-
 # #### don't run anything past here while we troubleshooting #### #
 streamlit.stop()
 
